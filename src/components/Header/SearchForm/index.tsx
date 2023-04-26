2bcbc3b1bd0cc25c3ddd005d4a5b979abf6d415b
Juliano <julianosudecum525@gmail.com>
2023-04-26 15:27:27 -0300

    save

--- src/components/Header/SearchForm/index.tsx
+++ src/components/Header/SearchForm/index.tsx
@@ -6,31 +6,11 @@ import { ProductsContext } from '../../../providers/ProductsContext';
 
 const SearchForm = () => {
 
-  const { products } = useContext(ProductsContext)
-  const { filterName, setFilterName } = useContext(ProductsContext)
-  const { filterCategory, setFilterCategory } = useContext(ProductsContext)
-
-  const [ valueInput , setValueInput ] = useState("")
-
-  const productsFilterName = products.filter(item => item.name.toLowerCase().includes(valueInput.toLowerCase()))
-  const productsFilterCategory = products.filter(item => item.category.toLowerCase().includes(valueInput.toLowerCase()))
-
-  const functionTeste = (e: ChangeEvent<HTMLInputElement>) => {
-    setValueInput(e.target.value)
-    setFilterName(productsFilterName)
-    setFilterCategory(productsFilterCategory)
-
-    if(e.target.value == ""){
-      setFilterCategory([])
-      setFilterName([])
-    }
-  }
+  const { searchProducts } = useContext(ProductsContext)
   
-  
-
   return(
     <StyledSearchForm>
-      <input onChange={(e) => functionTeste(e)} type='text' placeholder='Digitar pesquisa' />
+      <input onChange={(e) => searchProducts(e)} type='text' placeholder='Digitar pesquisa' />
       <StyledButton type='submit' $buttonSize='medium' $buttonStyle='green'>
         <MdSearch />
       </StyledButton>
--- src/providers/ProductsContext.tsx
+++ src/providers/ProductsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { ChangeEvent, createContext, useContext, useState } from "react";
 import { api } from "../services/api";
 import { iProduct } from "./CartContext";
 
@@ -16,6 +16,7 @@ interface iProductsCont{
   products: iProduct[]
   setProducts: React.Dispatch<React.SetStateAction<iProduct[]>>
   getProducts: () => Promise<void>
+  searchProducts: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
 export const ProductsProvider = ({children}: iProductsProps) =>{
@@ -23,7 +24,10 @@ export const ProductsProvider = ({children}: iProductsProps) =>{
   const [ filterName , setFilterName ] = useState<iProduct[]>([])
   const [ filterCategory , setFilterCategory ] = useState<iProduct[]>([])
   const [ products, setProducts ] = useState<iProduct[]>([])
+  const [ valueInput , setValueInput ] = useState("")
 
+  const productsFilterName = products.filter(item => item.name.toLowerCase().includes(valueInput.toLowerCase()))
+  const productsFilterCategory = products.filter(item => item.category.toLowerCase().includes(valueInput.toLowerCase()))
 
   const getProducts = async () =>{
     const token = localStorage.getItem("@hamburguer-token")
@@ -38,9 +42,20 @@ export const ProductsProvider = ({children}: iProductsProps) =>{
       console.log(error)
     }
   }
+
+  const searchProducts = (e: ChangeEvent<HTMLInputElement>) => {
+    setValueInput(e.target.value)
+    setFilterName(productsFilterName)
+    setFilterCategory(productsFilterCategory)
+
+    if(e.target.value == ""){
+      setFilterCategory([])
+      setFilterName([])
+    }
+  }
   
   return(
-    <ProductsContext.Provider value={{products, setProducts, getProducts, filterName, setFilterName, filterCategory, setFilterCategory}}>
+    <ProductsContext.Provider value={{searchProducts, products, setProducts, getProducts, filterName, setFilterName, filterCategory, setFilterCategory}}>
       {children}
     </ProductsContext.Provider>
   )
